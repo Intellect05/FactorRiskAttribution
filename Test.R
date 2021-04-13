133c115fddc43fa47963e244de98e78d355fa50e
Intellect05 <sanelemakamo26@gmail.com>
2021-04-13 12:05:33 +0200

    Update Test.R

--- Test.R
+++ Test.R
@@ -1,13 +1,15 @@
 
-
+## Time series of daily, weekly or monthly asset returns with the first column being the date 
 Stocks<-read_excel("\\path...file.xlsx", trim_ws = FALSE, col_names = TRUE, na ="")[,-1]
 
+## Time series of daily, weekly or monthly factor returns with the first column being the date 
 Factors<-read_excel(""\\path...file.xlsx"", trim_ws = FALSE, col_names = TRUE, na ="")[,-1]
 
+## Weights of the assets in the portfolio and index
 Weights<-read_excel(""\\path...file.xlsx"", trim_ws = FALSE, col_names = TRUE, na ="")
-
 
 source("r scrip path\\script.R")
 
+##Multi-Factor-Risk Model
 MultiFactor_Attr(Stocks = Stocks, Factors = Factors,PortfolioWeights = Weights[,4],IndexWeights = Weights[,5],ActiveWeights = Weights[,6],
                  method =c("Factor risk attribution"), scale = 52, drop_factor =0)
