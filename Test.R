6876555277b277441429048c42e8cf03126d8ebd
Sanele E. Makamo <sanelemakamo26@gmail.com>
2021-04-13 15:22:04 +0200

    Test

--- Test.R
+++ Test.R
@@ -10,6 +10,6 @@ Weights<-read_excel(""\\path...file.xlsx"", trim_ws = FALSE, col_names = TRUE, n
 
 source("r scrip path\\script.R")
 
-##Multi-Factor-Risk Model
+##Multi-Factor-Risk Model illustration
 MultiFactor_Attr(Stocks = Stocks, Factors = Factors,PortfolioWeights = Weights[,4],IndexWeights = Weights[,5],ActiveWeights = Weights[,6],
                  method =c("Factor risk attribution"), scale = 52, drop_factor =0)
