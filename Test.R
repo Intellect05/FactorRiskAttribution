c72383de2be7a1969847baa03e53055f196ea39a
Intellect05 <sanelemakamo26@gmail.com>
2021-04-13 13:43:56 +0200

    Test

--- Test.R
+++ Test.R
@@ -1,9 +1,9 @@
 
 ## Time series of daily, weekly or monthly asset returns with the first column being the date 
-Stocks<-read_excel("\\path...file.xlsx", trim_ws = FALSE, col_names = TRUE, na ="")[,-1]
+Stocks<-read_excel("\\path...file.xlsx", trim_ws = FALSE, col_names = TRUE, na ="")[,-1] ##drop Date Column
 
 ## Time series of daily, weekly or monthly factor returns with the first column being the date 
-Factors<-read_excel(""\\path...file.xlsx"", trim_ws = FALSE, col_names = TRUE, na ="")[,-1]
+Factors<-read_excel(""\\path...file.xlsx"", trim_ws = FALSE, col_names = TRUE, na ="")[,-1] ##drop Date Column
 
 ## Weights of the assets in the portfolio and index
 Weights<-read_excel(""\\path...file.xlsx"", trim_ws = FALSE, col_names = TRUE, na ="")
